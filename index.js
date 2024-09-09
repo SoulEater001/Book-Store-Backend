feb6b0579b05e206a9f8d093e0028c596b5decf7
SoulEater001 <dshivam698@gmail.com>
2024-09-09 17:46:16 +0530

    Changed cors policy origin

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ app.use(express.json());
 
 //Middleware for handling cors policy
 //Allowing all origins with default of cors(*)
-app.use(cors());
+// app.use(cors());
 
 //Allowing custom origin
-// app.use(cors({
-//     origin: 'http://localhost/3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type']
-// }))
+app.use(cors({
+    origin: 'https://book-store-souleater.netlify.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}))
 
 app.get('/', (request, response) => {
     console.log(request);
